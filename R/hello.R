21cffe3cdb23a88e8209094efacc047b6f09d55e
etiennebacher <etienne.bacher@protonmail.com>
2022-08-30 17:29:24 +0200

    clean

--- R/hello.R
+++ R/hello.R
@@ -1,9 +1,8 @@
 #' @export
 foo <- function() {
-  tryCatch({
-    if (c(TRUE, TRUE) || c(TRUE, TRUE)) 1
-  },
-  warning = function(w) NULL,
-  error = function(e) NULL
+  tryCatch(
+    if (c(TRUE, TRUE) || c(TRUE, TRUE)) 1,
+    warning = function(w) NULL,
+    error = function(e) NULL
   )
 }
