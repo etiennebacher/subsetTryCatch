31034a128906d331b612f33bf3e14c28be2eee25
etiennebacher <etienne.bacher@protonmail.com>
2022-09-01 14:55:56 +0200

    simplify package

--- tests/testthat/test-hello.R
+++
@@ -1,5 +0,0 @@
-test_that("", {
-  expect_null(
-    foo()
-  )
-})
--- tests/tinytest.R
+++ tests/tinytest.R
@@ -1,5 +1,5 @@
 
 if ( requireNamespace("tinytest", quietly=TRUE) ){
-  tinytest::test_package("subsetTryCatch")
+  tinytest::test_package("testpackage")
 }
 
